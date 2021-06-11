3656bc73d655f79c64cde82f263e6f5b55e4b4c4
pierreGarciaRaf <56490823+pierreGarciaRaf@users.noreply.github.com>
2021-06-11 23:58:48 +0200

    Initial commit

---
+++ src/world/player/player.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+export var speed = 100
+
+func _physics_process(delta):
+	var xAxis = int(Input.is_action_pressed("go_right")) - int(Input.is_action_pressed("go_left"))
+	var yAxis = int(Input.is_action_pressed("go_down")) - int(Input.is_action_pressed("go_up"))
+	
+	self.move_and_slide(Vector2(xAxis,yAxis).normalized() * speed)
---
+++ src/world/player/playerSTM.gd
@@ -0,0 +1,25 @@
+extends StateMachine
+
+func _ready():
+	add_state("walkNorthSide")
+	add_state("walkSouthSide")
+	add_state("idle")
+	call_deferred("set_state",states.idle)
+
+	
+
+
+
+
+func _enter_state(new_state,previous_state):
+	if new_state != previous_state and previous_state != null:
+		print(previous_state," -> ", new_state)
+
+func _get_transition(_delta):
+	pass
+
+
+
+func _state_logic(delta):
+	pass
+
