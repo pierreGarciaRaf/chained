11aa15e4d0ab297a7ca085dcefaab1b6279f84c5
Jean-Baptiste-Garcia <garcia.jean-baptiste@orange.fr>
2021-06-12 22:01:36 +0200

    LevelSelector from cooc

---
+++ src/levelSelector/buttonToLevel.gd
@@ -0,0 +1,15 @@
+extends Button
+
+var levelNumber
+func get_level_number():
+	return int((self.name as String).trim_prefix("buttonToLevel"))
+
+func _ready(): 
+	levelNumber = get_level_number()
+	self.disabled = not LevelChangeManager.unlockedLevelNumber(levelNumber)
+	self.text = "      Level " + String(levelNumber) + "     "
+
+
+
+func _on_buttonToLevel_pressed():
+	LevelChangeManager.changeToLevel(levelNumber)
