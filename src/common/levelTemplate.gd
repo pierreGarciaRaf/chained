a9484b11b834b41514167c8f04b591eeece11ee6
Jean-Baptiste-Garcia <garcia.jean-baptiste@orange.fr>
2021-06-13 10:47:40 +0200

    add safe place to guillotine

--- src/common/levelTemplate.gd
+++ src/common/levelTemplate.gd
@@ -20,15 +20,24 @@ func _ready():
 	(tileMapForLightRcast).tile_set = preload("res://src/world/environment/walls&Floor/blueTilesetLightCollisions.tres")
 	tileMap.get_parent().add_child(tileMapForLightRcast)
 	
+	# Referencing key children
 	player=get_node('player')
 	monster=get_node('monster')
 	
 	guillotine = get_node("Guillotine")
-	guillotine.connect('guillotine_entered',self,'on_guillotine_entered')
+	if guillotine:
+		guillotine.connect('guillotine_entered',self,'on_guillotine_entered')
+		guillotine.connect('safe_place_entered',self,'on_safe_place_entered')
+	else:
+		print('WARNING : no guillotine')
+		
 
 func on_guillotine_entered(body):
 	print('body entered guillotine', body)
 	if body== player:
-		print('Player win')
+		print('Player about to win')
 	if body== monster:
-		print('Monster win')
+		print('Monster killed')
+		
+func on_safe_place_entered(body):
+	print('Player wins; cut the chain')
--- src/world/environment/guillotine/Guillotine.gd
+++ src/world/environment/guillotine/Guillotine.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 signal guillotine_entered(body)
+signal safe_place_entered(body)
 
-
-func _on_Area2D_body_entered(body):	
+func _on_Guillotine_entered(body):
 	emit_signal("guillotine_entered",body)
+
+
+func _on_SafePlace_body_entered(body):
+	emit_signal("safe_place_entered",body)
